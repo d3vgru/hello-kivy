64da76860e8e7b192319884bb350d6c4d1d30301
Ed Knutson <knutsoned@gmail.com>
2013-03-06 16:51:45 -0600

    fixed runtime error

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ class Controller(FloatLayout):
     otherlabel_wid = ObjectProperty()
     info = StringProperty()
     moreinfo = StringProperty()
-    
-    #TODO some M2Crypto stuff that sets moreinfo
-    self.moreinfo = 'Change to this'
 
     # handle button press
     def do_action(self):
-        self.label_wid.text = 'My label after button press'
-        self.otherlabel_wid.txt = self.moreinfo
-        self.info = 'New info text'
+	    #TODO some M2Crypto stuff that sets moreinfo
+    	self.moreinfo = 'Change to this'
 
+        self.label_wid.text = 'My label after button press'
+        self.otherlabel_wid.text = self.moreinfo
+        self.info = 'New info text'
+	    
 
 class ControllerApp(App):
     def build(self):
